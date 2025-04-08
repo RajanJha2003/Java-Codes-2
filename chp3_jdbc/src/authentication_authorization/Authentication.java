34b1975bbeed3051eedc41ac7d60a01d61e441b5
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-08 11:47:25 +0530

    authentication

---
+++ chp3_jdbc/src/authentication_authorization/Authentication.java
@@ -0,0 +1,30 @@
+package authentication_authorization;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class Authentication {
+  public static String authenticateUser(String username,String password) {
+	  String role="";
+	  
+	  try {
+		String sql="select * from users where username=? and password=?";
+		PreparedStatement preparedStatement=DBConnection.getConnection().prepareStatement(sql);
+		preparedStatement.setString(1, username);
+		preparedStatement.setString(2, password);
+		
+		ResultSet resultSet=preparedStatement.executeQuery();
+		
+		if(resultSet.next()) {
+			role=resultSet.getString("role");
+		}
+	} catch (Exception e) {
+		// TODO: handle exception
+		e.printStackTrace();
+		System.out.println(e);
+	}
+	  
+	  return role;
+	  
+  }
+}
