8835b97b66cb2baa12ff326b3231e8e6a34e719e
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-08 16:11:50 +0530

    swing jdbc

---
+++ chp32_jdbc_swing/src/operations/Operations.java
@@ -0,0 +1,14 @@
+package operations;
+
+import java.util.List;
+
+import model.User;
+
+public interface Operations {
+	 boolean addUser(User user);
+	 List<User> getAllUsers();
+	 boolean updateUser(User user);
+	 boolean deleteUser(int id);
+	 User searchUser(int id);
+	 boolean loginUser(String email, String password);
+}
