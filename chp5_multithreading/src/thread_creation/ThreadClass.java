12a831cc9f830399decd82765fdf58298f91848f
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-14 12:31:08 +0530

    thread creation using thread class

---
+++ chp5_multithreading/src/thread_creation/ThreadClass.java
@@ -0,0 +1,55 @@
+package thread_creation;
+class MyThread1 extends Thread{
+	
+	
+	@Override
+	public void run() {
+	
+		
+		for(int i=0;i<5;i++) {
+			System.out.println(Thread.currentThread().getName());
+			System.out.println(i);
+		}
+	}
+}
+
+class MyThread2 extends Thread{
+	
+	
+	@Override
+	public void run() {
+	
+		
+		for(int i=0;i<5;i++) {
+			System.out.println(Thread.currentThread().getName());
+			System.out.println(i);
+		}
+	}
+}
+class MyThread3 extends Thread{
+	
+	
+	@Override
+	public void run() {
+		
+		
+		for(int i=0;i<5;i++) {
+			System.out.println(Thread.currentThread().getName());
+			System.out.println(i);
+		}
+	}
+}
+
+
+public class ThreadClass {
+	public static void main(String[] args) {
+		MyThread1 myThread1=new MyThread1();
+		MyThread2 myThread2=new MyThread2();
+		MyThread3 myThread3=new MyThread3();
+		
+		myThread1.start();
+		myThread2.start();
+		myThread3.start();
+	}
+
+}
