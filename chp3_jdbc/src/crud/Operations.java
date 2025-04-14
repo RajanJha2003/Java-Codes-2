87ec2f3e57cc8d791ba2989c3396c34b09921e7c
RajanJha2003 <rajanj448@student.sfit.ac.in>
2025-04-14 09:37:48 +0530

    add user

--- chp3_jdbc/src/crud/Operations.java
+++ chp3_jdbc/src/crud/Operations.java
@@ -1,5 +1,12 @@
 package crud;
 
+import java.util.List;
+
 public interface Operations {
+	boolean addUser(User user);
+	 List<User> getAllUsers();
+	 boolean updateUser(User user);
+	 boolean deleteUser(int id);
+	 User searchUser(int id);
 
 }
--- chp3_jdbc/src/crud/OperationsImp.java
+++ chp3_jdbc/src/crud/OperationsImp.java
@@ -1,5 +1,70 @@
 package crud;
 
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OperationsImp implements Operations {
+
+	@Override
+	public boolean addUser(User user) {
+		// TODO Auto-generated method stub
+		boolean success=false;
+		try {
+			String sql="insert into users(username,password,email,phone,gender,city) values(?,?,?,?,?,?)";
+			PreparedStatement preparedStatement=DbConnection.getConnection().prepareStatement(sql);
+			preparedStatement.setString(1, user.getUsername());
+			preparedStatement.setString(2, user.getPassword());
+			preparedStatement.setString(3, user.getEmail());
+			preparedStatement.setString(4, user.getPhone());
+			preparedStatement.setString(5, user.getGender());
+			preparedStatement.setString(6, user.getCity());
+			
+			int add=preparedStatement.executeUpdate();
+			if(add==1) {
+				success=true;
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			System.out.println(e);
+		}
+		return success;
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		// TODO Auto-generated method stub
+		List<User> list=new ArrayList<User>();
+		User user=null;
+		try {
+			String sql="select * from users";
+			PreparedStatement preparedStatement=DbConnection.getConnection().prepareStatement(sql);
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return null;
+	}
+
+	@Override
+	public boolean updateUser(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public User searchUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
